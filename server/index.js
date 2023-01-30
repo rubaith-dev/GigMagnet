571958a678b9252cc80ec35cd47274bc8ac388a8
Rubaith <dev.rubaith@gmail.com>
2023-01-30 15:12:07 +0600

    prisma initialization done

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.use(
+  cors({
+    origin: [process.env.PUBLIC_URL],
+    method: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
