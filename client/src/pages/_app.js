ba3079545f240144a3942460c5353d42cb561dfe
Rubaith <dev.rubaith@gmail.com>
2023-02-01 15:29:06 +0600

    static hero section is completed

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Work_Sans } from "next/font/google";
+
+const workSans = Work_Sans({subsets:['latin']})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={workSans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
